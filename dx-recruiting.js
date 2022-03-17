3d4c5611829101ad26d05f5cb13b43ea2908cbbc
L30M31M4Nn <leojmelman@gmail.com>
2022-03-17 09:30:13 -0300

    Final Code

--- dx-recruiting.js
+++ dx-recruiting.js
@@ -1,9 +1,13 @@
 function longestWordA(str){
+    //Split the string by space or comma and then execute reduce function
     return str.split(/[, ]+/).reduce( (prev, curr) => {
+        //Check each word length
         if(prev.length < curr.length){
             prev = curr
         }
+        //If the same length
         else if(prev.length == curr.length){
+            //Check number of vowels 
             var vowelsA = prev.match(/[aeiou]/gi).length;
             var vowelsB = curr.match(/[aeiou]/gi).length;
             if(vowelsB > vowelsA){
